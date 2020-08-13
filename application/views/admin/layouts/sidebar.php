0c377f735b0100cb84568578d3d8570947f5f6ed
as-shiddiq <n.shiddiq@gmail.com>
2020-08-13 13:25:59 +0800

    MENCOBA MENJAWAB PART 10 - Mengubah Warna Polygon dari CSV (Leaflet)

--- application/views/admin/layouts/sidebar.php
+++ application/views/admin/layouts/sidebar.php
@@ -47,6 +47,11 @@
                       <li><a href="<?=site_url('admin/leafletpolygon')?>">Polygon</a></li>
                     </ul>
                   </li>
+                   <li><a><i class="fa fa-question"></i> Mencoba Menjawab <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="<?=site_url('admin/mmbagian10')?>">Bagian 10</a></li>
+                    </ul>
+                  </li>
                   <li><a href="<?=site_url('admin/auth/out')?>"><i class="fa fa-sign-out"></i> Keluar</a></li>
                 </ul>
               </div>
